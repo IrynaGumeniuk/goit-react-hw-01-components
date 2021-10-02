68aadf471ddf5f99f20fe13a500c91d3a1bc2656
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-10-03 01:10:51 +0200

    tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from 'react';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-function App() {
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+const App = () => {
   return (
-    <div className="App">
-      <p>
-        Привет
-      </p>
+    <div className="wrap">
+      <h1>Home Work #1</h1>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
-}
+};
 
 export default App;
